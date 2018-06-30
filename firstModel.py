4cb6cff7673addff4b76b6808d4b9907a056a27b
Matteo Mantese <mavese@live.com>
2018-06-30 11:54:32 -0500

    Switched to onehot encoding and recieved first accuracy of 82%.

--- firstModel.py
+++ firstModel.py
@@ -12,13 +12,16 @@ def main():
     df = df.iloc[:,0:19].dropna().reindex()
 
     # declaring encoding functions to turn strings to integers
-    def labelEncoder(r,column):
+    def labelOneHotEncoder(r,column):
         if r == 'win_A':
-            return 0
+            switcher = {1:1, 2:0, 3:0}
+            return switcher.get(column, 0)
         elif r == 'win_B':
-            return 1
+            switcher = {1:0, 2:1, 3:0}
+            return switcher.get(column, 0)
         else:
-            return 2
+            switcher = {1:0, 2:0, 3:1}
+            return switcher.get(column, 0)
 
     def stanceEncoder(r):
         if r == 'orthodox':
@@ -27,17 +30,19 @@ def main():
             return 1
 
     # encoding strings in the data, the label and the stance columns
-    df['label'] = df['result'].apply(labelEncoder)
+    df['label_1'] = df['result'].apply(lambda r: labelOneHotEncoder(r,1))
+    df['label_2'] = df['result'].apply(lambda r: labelOneHotEncoder(r,2))
+    df['label_3'] = df['result'].apply(lambda r: labelOneHotEncoder(r,3))
     df['stance_A'] = df['stance_A'].apply(stanceEncoder)
     df['stance_B'] = df['stance_B'].apply(stanceEncoder)
 
     # seperate features and labels
     data = df.iloc[:,0:18]
-    labels = df.iloc[:,19]
+    labels = df.iloc[:,19:22]
 
     # convert pandas dataframe's to numpy nDarray's
     npData = data.values
-    npLabels = labels.values.reshape((-1,1))
+    npLabels = labels.values
 
     # randomly select 80% of the data to train on
     randomArr = np.random.random_integers(0, high=npData.shape[0]-1, size=int(round(.8*npData.shape[0])))
@@ -58,7 +63,7 @@ def trainNetwork(xTrain, yTrain):
 
     # setting up the model, compiling and fitting to the given data
     model = tf.keras.models.Model(inputs=inputs, outputs=output)
-    model.compile(optimizer='sgd', loss=tf.keras.losses.binary_crossentropy, metrics=['accuracy'])
+    model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
     model.fit(x=xTrain, y=yTrain, batch_size=32, epochs=10)
 
     return model
